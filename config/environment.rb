bdfee8b7ec90666a8670a46597977eabe314d93f
Zach Pardee <zachpardee@gmail.com>
2019-03-26 11:27:44 -0600

    added players and characters to env

--- config/environment.rb
+++ config/environment.rb
@@ -9,23 +9,61 @@ adapter: "sqlite3",
 database: "db/fantasy.db"
 )
 
-dany = Character.create(name:"Dany", status: "alive")
-john = Character.create(name:"John Snow", status: "alive")
+
 yara = Character.create(name:"Yara Greyjoy", status: "alive")
-theon =Character.create(name:"Theon Greyjoy", status: "alive")
+theon = Character.create(name:"Theon Greyjoy", status: "alive")
 mountain = Character.create(name:"The Mountain", status: "alive")
+euron = Character.create(name:"Euron Greyjoy", status: "alive")
+tormund = Character.create(name:"Tormund Giantsbane", status: "alive")
+bronn = Character.create(name:"Bronn", status: "alive")
+hound = Character.create(name:"The Hound", status: "alive")
+gendry = Character.create(name:"Gendry", status: "alive")
+jorah = Character.create(name:"Jorah Mormont", status: "alive")
+missandei = Character.create(name:"Missandei", status: "alive")
+podrick = Character.create(name:"Podrick Paine", status: "alive")
+greyworm = Character.create(name:"Grey Worm", status: "alive")
+davos = Character.create(name:"Davos Seaworth", status: "alive")
+brienne = Character.create(name:"Brienne", status: "alive")
+cercei = Character.create(name:"Cercei Lannister", status: "alive")
+nightking = Character.create(name:"The Night King", status: "alive")
+varys = Character.create(name:"Varys", status: "alive")
+arya = Character.create(name:"Arya Stark", status: "alive")
+samwell = Character.create(name:"Samwell Tarly", status: "alive")
+sansa = Character.create(name:"Sansa Stark", status: "alive")
+melisandre = Character.create(name:"Melisandre", status: "alive")
+tyrion = Character.create(name:"Tyrion Lannister", status: "alive")
+bran = Character.create(name:"Bran Stark", status: "alive")
+jamie = Character.create(name:"Jaime Lannister", status: "alive")
+john = Character.create(name:"John Snow", status: "alive")
+dany = Character.create(name:"Daenerys Targaryen", status: "alive")
+
 
 zach = Player.create(name:"Zach", score: 3)
 kendall = Player.create(name:"Kendall", score: 3)
 zack = Player.create(name:"Zack", score: 3)
 jack = Player.create(name:"Jack", score: 3)
 kevin = Player.create(name:"Kevin", score: 3)
-jard = Player.create(name:"Jared", score: 3)
+jared = Player.create(name:"Jared", score: 3)
+ben = Player.create(name:"Ben", score: 3)
+kari = Player.create(name:"Kari", score: 3)
+crytal = Player.create(name:"Crystal", score: 3)
 sarah = Player.create(name:"Sarah", score: 3)
 
 
-def draft_player(player, character)
+def draft_player(player, character, predictedstatus)
   PlayerCharacter.create(player: player, character: character)
+end
+
+draft_player(zach, sansa, "dead")
+draft_player(zach, sansa, "alive")
+
+def calculate_total_score(player)
+
+end
+
+
+def kill_character(character)
+
 end
 
 binding.pry
