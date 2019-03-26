d3dfaad7dad70fa4e6a732c1018a6421e0d5a60a
github username <github email address>
2019-03-26 15:27:20 -0600

    added a working.. but not verified version of calculating the final score

--- lib/models/player_character.rb
+++ lib/models/player_character.rb
@@ -1,4 +1,21 @@
 class PlayerCharacter < ActiveRecord::Base
   belongs_to :player
   belongs_to :character
+
+
+
+  def draft_player(player, character, predictedstatus)
+     PlayerCharacter.create(player: player, character: character)
+   end
+
+  def calculate_total_score(player: player)
+    player_characters_array = PlayerCharacter.where(player: player)
+    player_characters_array.select do |playercharacter| playercharacter.predictedstatus == "alive"
+      self.player.score += 1
+    end
+    player_characters_array.select do |playercharacter| playercharacter.predictedstatus == "dead"
+      self.player.score -= 1
+    end
+  end
+
 end
