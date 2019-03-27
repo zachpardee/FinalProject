13bb17f2f43e7353c86761863d538d205e061322
Zach Pardee <zachpardee@gmail.com>
2019-03-27 10:39:01 -0600

    added CLI draft selection code

--- config/environment.rb
+++ config/environment.rb
@@ -15,17 +15,26 @@ def play_game
   PlayerCharacter.destroy_all
 
   # First Draft Pick
-  puts "Please enter your name: "
-  response = gets.chomp
-  player = Player.find_by(name:response)
-  puts "Enter your character selection: "
-  character = gets.chomp()
-  puts "alive or dead?"
-  dead_or_alive = gets.chomp
-  PlayerCharacter.create(
-    player: player,
-    character: Character.find_by(name:character),
-    predictedstatus: dead_or_alive)
+
+  5.times do
+    Player.all.each do |player|
+      puts player.name
+    end
+    puts "\nPlease enter your name: "
+    response = gets.chomp
+    player = Player.find_by(name:response)
+    Character.all.each do |character|
+      puts character.name
+    end
+    puts "\nEnter your character selection: "
+    character = gets.chomp()
+    puts "alive or dead?"
+    dead_or_alive = gets.chomp
+    PlayerCharacter.create(
+      player: player,
+      character: Character.find_by(name:character),
+      predictedstatus: dead_or_alive)
+  end
 
 end
 
